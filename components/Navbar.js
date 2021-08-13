c7263aca29a15d744caa43126fac63e8bb2b790a
Manibarathi <49336839+mani-barathi@users.noreply.github.com>
2021-08-13 20:27:49 +0530

    navbar sticky

--- components/Navbar.js
+++ components/Navbar.js
@@ -3,16 +3,16 @@ import CustomLink from "./common/CustomLink";
 
 function Navbar() {
   return (
-    <nav className="flex justify-end w-full p-4 pb-5">
+    <nav className="bg-white z-50 sticky top-0 flex justify-end w-full p-4">
       <CustomLink
         href="/"
-        className="font-medium text-gray-700 hover:text-gray-900"
+        className="ml-3 sm:ml-5 font-medium text-gray-700 hover:text-gray-900"
       >
         Home
       </CustomLink>
       <CustomLink
         href="/about"
-        className="ml-2 sm:ml-5 font-medium text-gray-700 hover:text-gray-900"
+        className="ml-3 sm:ml-5 font-medium text-gray-700 hover:text-gray-900"
       >
         About
       </CustomLink>
