b5030038ddcc254c22543e62da9fb2d33f6abd07
Manibarathi <49336839+mani-barathi@users.noreply.github.com>
2021-09-23 20:06:32 +0530

    added login button in about page

--- pages/message.js
+++ pages/message.js
@@ -9,8 +9,24 @@ function message() {
           Leave a message
         </h1>
         <p className="mt-4 mb-4 text-gray-600 sm:text-lg">
-          Oops! sorry, this page is under development.
+          Whether you're interested in working on a project together or just
+          want to say hello, feel free to leave a message and I'll get back to
+          you as soon as I can.
         </p>
+        <div className="p-3 mt-3 text-gray-700 bg-blue-100 rounded-md border border-blue-200">
+          <p className="sm:text-lg">
+            Login with your <span className="text-blue-500 font-medium">G</span>
+            <span className="text-red-500 font-medium">o</span>
+            <span className="text-yellow-500 font-medium">o</span>
+            <span className="text-blue-500 font-medium">g</span>
+            <span className="text-green-600 font-medium">l</span>
+            <span className="text-red-500 font-medium">e</span> account to send
+            a message.
+          </p>
+          <button className="mt-3 py-1 px-5 font-medium text-white bg-blue-500 transition rounded transform active:translate-y-0.5 hover:bg-blue-600">
+            Login
+          </button>
+        </div>
       </div>
     </PageContainer>
   );
