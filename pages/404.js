056f6d26b59e24844a904f0889178bf6812ebf17
Manibarathi <49336839+mani-barathi@users.noreply.github.com>
2021-09-27 23:03:58 +0530

    added 404 page

---
+++ pages/404.js
@@ -0,0 +1,18 @@
+import PageContainer from "../layout/PageContainer";
+
+export default function PageNotFound404() {
+  return (
+    <PageContainer>
+      <div className="flex-grow flex flex-col justify-center items-center select-none">
+        <h1 className="text-8xl font-md text-gray-600">404</h1>
+        <h3 className="my-3 text-3xl font-md text-gray-500">
+          UH OH! You're lost.
+        </h3>
+        <p className="text-gray-500 max-w-md text-center">
+          The page you are looking for does not exist. How you got here is a
+          mystery.
+        </p>
+      </div>
+    </PageContainer>
+  );
+}
