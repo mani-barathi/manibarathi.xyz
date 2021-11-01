4cf966c3383cca9cd854d3501f36cbafc3d550d1
Manibarathi <49336839+mani-barathi@users.noreply.github.com>
2021-11-01 10:43:42 +0530

    added NProgress for page loading indication

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,13 @@
 import "../styles/global.css";
+import NProgress from "../components/Nprogress";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Component {...pageProps} />
+      <NProgress />
+    </>
+  );
 }
 
 export default MyApp;
