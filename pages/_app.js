0eace6878f429c2632f31950d35bef8b00d4fa58
Manibarathi <49336839+mani-barathi@users.noreply.github.com>
2022-01-31 12:58:56 +0530

    added Google Analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,24 @@
+import Script from "next/script";
 import "../styles/global.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Script
+        stratergy="lazyOnload"
+        src={`https://www.googletagmanager.com/gtag/js?id=G-${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+      />
+      <Script stratergy="lazyOnload">
+        {`window.dataLayer = window.dataLayer || [];
+       function gtag(){dataLayer.push(arguments);}
+       gtag('js', new Date());
+
+       gtag('config', 'G-${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}'); 
+        `}
+      </Script>
+      <Component {...pageProps} />;
+    </>
+  );
 }
 
 export default MyApp;
