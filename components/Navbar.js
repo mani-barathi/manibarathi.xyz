af785527a4cb9df7911bbd7a02f4835e4ce3c8b4
Manibarathi <49336839+mani-barathi@users.noreply.github.com>
2022-02-07 12:23:49 +0530

    updated Navbar Link hover state & added image in About Page

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,24 +1,29 @@
-import React from "react";
+import { useRouter } from "next/router";
 import CustomLink from "./CustomLink";
 
 function Navbar() {
+  const router = useRouter();
+  const isActive = (href) => router.asPath === href;
+  const classes =
+    "transition-all duration-75 ml-3 sm:ml-5 text-gray-700 tracking-wide border-b-4 border-transparent hover:text-gray-800 hover:border-b-2 hover:border-gray-600";
+
   return (
     <nav className="bg-gray-50 z-50 sticky top-0 flex justify-end w-full p-4">
       <CustomLink
         href="/"
-        className="ml-3 sm:ml-5 font-normal text-gray-600 hover:text-gray-800 tracking-wide sm:text-lg"
+        className={`${classes} ${isActive("/") && "font-semibold"}`}
       >
         Home
       </CustomLink>
       <CustomLink
         href="/blog"
-        className="ml-3 sm:ml-5 font-normal text-gray-600 hover:text-gray-800 tracking-wide sm:text-lg"
+        className={`${classes} ${isActive("/blog") && "font-semibold"}`}
       >
         Blog
       </CustomLink>
       <CustomLink
         href="/about"
-        className="ml-3 sm:ml-5 font-normal text-gray-600 hover:text-gray-800 tracking-wide sm:text-lg"
+        className={`${classes} ${isActive("/about") && "font-semibold"}`}
       >
         About
       </CustomLink>
--- pages/about.js
+++ pages/about.js
@@ -1,4 +1,5 @@
-// import Image from "next/image";
+import React from "react";
+import Image from "next/image";
 import PageContainer from "../components/PageContainer";
 
 function about() {
@@ -13,25 +14,29 @@ function about() {
       <h1 className="font-bold text-gray-800 text-3xl sm:text-5xl tracking-tight mb-4">
         About
       </h1>
-      {/* <Image
-        src="https://avatars.githubusercontent.com/u/49336839?s=460&u=fbbc21b3ee2066b82cf7ddf1205524757ac5f3f4&v=4"
-        height="200"
-        width="200"
-        alt="image"
-        className="rounded-lg sm:mr-8"
-          /> */}
 
-      <p className="text-gray-600 sm:text-lg">
-        Hey, I'm Mani. I'm an MCA student at VIT, Vellore. Apart from college, I
-        spend my free time speedcubing and building personal projects. I also
-        teach coding and write articles.
-      </p>
-
-      <p className="mt-3 text-gray-600 sm:text-lg">
-        I grew up in Chennai, spending my school days playing football and
-        hanging out with friends. I also hold a bachelor's degree in computer
-        science.
-      </p>
+      <div className="flex flex-col-reverse sm:flex-row items-start">
+        <div className="flex flex-col pr-8">
+          <p className="text-gray-600 sm:text-lg max-w-xl">
+            Hey, I'm Mani. I'm an MCA student at VIT, Vellore. Apart from
+            college, I spend my free time speedcubing and building personal
+            projects. I also teach coding and write articles.
+          </p>
+          <p className="mt-2 text-gray-600 sm:text-lg max-w-xl">
+            I grew up in Chennai, spending my school days playing football and
+            hanging out with friends. I also hold a bachelor's degree in
+            computer science.
+          </p>
+        </div>
+        <div className="w-[150px] sm:w-[250px]  sm:mb-0">
+          <Image
+            src="/images/mani.png"
+            height={250}
+            width={250}
+            className="rounded-full"
+          />
+        </div>
+      </div>
 
       <div className="mt-3">
         <h3 className="text-gray-800 text-2xl font-bold sm:text-3xl">
